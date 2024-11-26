aa8625acfb2b170ef49449bd3df4c317f8f34955
TEUJIANWEE <teujianwee@gmail.com>
2024-11-26 16:37:19 +0800

    Commit

---
+++ Person.java
@@ -0,0 +1,13 @@
+public class Person {
+    
+    public static void main (String[] args) {
+        String name = "Tan";
+        int age = 20;
+        double height = 1.75;
+        boolean isStudent= true;
+        long networth = 2500000000L;
+        System.out.println("The student name is : " + name +" and age is: "+age);
+        System.out.println("The student height is : " + height + "and is a student: "+isStudent);
+        System.out.println("The student networth is : " + networth + "RM");
+    }
+}
